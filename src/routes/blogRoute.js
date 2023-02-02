ac222223bb66fc408cc15ce054daf0cf8e110ad4
Tparfaite <parfaitetwagira@gmail.com>
2023-02-02 21:07:41 +0200

    fix admin [Finishes#]

--- src/routes/blogRoute.js
+++ src/routes/blogRoute.js
@@ -3,13 +3,16 @@ import BlogController from "../controller/blogController";
 // import validatedBlog from "../middleware/validatedBlog";
 import verifyToken from "../middleWare/authenticate";
 import upload from "../upload/multer";
+import { isAdmin,findUser } from "../middleware/isAdmin";
 
 
 const BlogRoute=express.Router();
 
-BlogRoute.post("/createBlog",verifyToken,upload.single('blogImage'),BlogController.createBlog);
+BlogRoute.post("/createBlog/:id",verifyToken,findUser,isAdmin,upload.single('blogImage'),BlogController.createBlog);
 BlogRoute.get("/getAllBlogs",BlogController.retrieveAllBlog);
 BlogRoute.get("/getOneBlog/:id",BlogController.getSingleBlog);
-BlogRoute.get("/deleteBlog/:id",verifyToken,BlogController.deleteBlog);
+BlogRoute.get("/deleteBlog/:id",verifyToken,findUser,isAdmin,BlogController.deleteBlog);
+BlogRoute.put("/updateBlog/:id",verifyToken,upload.single('blogImage',BlogController.updateBlog));
 
 export default BlogRoute;
+
